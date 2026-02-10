a8895286bf7ac6f3f61f06df8cb8efee3dd5e27c
ctf2090 <ctf2090@gmail.com>
2026-02-10 18:58:14 +0800

    Add sample story building DB entry

--- tools/gen_story_building_db.gd
+++ tools/gen_story_building_db.gd
@@ -9,6 +9,16 @@ func _initialize() -> void:
 	var db := StoryBuildingDB.new()
 	db.entries = []
 
+	# Sample entry so the pipeline works out of the box.
+	# Paint a story entrance at this cell in the world to test.
+	var e := StoryBuildingEntry.new()
+	e.world_cell = Vector2i(21, 15)
+	e.interior_scene_path = "res://scenes/interiors/interior_template.tscn"
+	e.interior_spawn_cell = Vector2i(8, 8)
+	e.return_scene_path = "res://main.tscn"
+	e.return_cell = e.world_cell
+	db.entries.append(e)
+
 	var err := ResourceSaver.save(db, OUT_DB)
 	if err != OK:
 		push_error("Failed to save DB to %s (err=%s)" % [OUT_DB, str(err)])
@@ -21,4 +31,3 @@ func _initialize() -> void:
 func _ensure_dir(res_path: String) -> void:
 	var abs := ProjectSettings.globalize_path(res_path)
 	DirAccess.make_dir_recursive_absolute(abs)
-
